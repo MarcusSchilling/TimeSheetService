909f7e61d3f8712289c3e11f084172480e7788c9
Marcus Schilling <marcus@5-schillinge.de>
2020-06-01 20:54:33 +0200

    added tests unit as well as integration tests

---
+++ src/test.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    //use super::*;
+
+    #[test]
+    fn test_eq() {
+        assert_eq!(3, 3);
+    }
+
+    #[test]
+    fn tests_eq() {
+        // This assert would fire and test will fail.
+        // Please note, that private functions can be tested too!
+        assert_eq!(2, 2);
+    }
+}
